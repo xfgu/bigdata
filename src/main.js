e5a434fa5242b47111fa5107ee584a923503cecc
xfgu <632167902@qq.com>
2019-07-03 14:32:09 +0800

    配置好了后台框架

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import store from './store'
-
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/style/reset.css'
+import './assets/style/element.styl'
+import './assets/font/iconfont.css'
 Vue.config.productionTip = false
-
+Vue.use(Element)
 new Vue({
   router,
   store,
